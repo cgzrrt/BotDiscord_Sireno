c858490107d6dd8c9ab4f963631865404bae075e
kangaroo2121 <gonzalezcerratocelia@gmail.com>
2026-07-14 19:24:19 +0200

    Kick arreglado y Ban creado

--- sireno/cogs/moderation_cog.py
+++ sireno/cogs/moderation_cog.py
@@ -21,13 +21,25 @@ class ModerationCog(commands.Cog):
     # Expulsa a un miembro sin banearlo
     @commands.command()
     @commands.has_permissions(kick_members=True)
-    async def kickMember(self, ctx, member: discord.Member, reason: str | None = "No se dio razón"):
+    async def kick(self, ctx, member: discord.Member, reason: str | None = "No se dio razón"):
         await member.kick(reason=reason)
-        await ctx.send(f'El usuario {member} ha sido expulsado')
-    @kickMember.error
+        await ctx.send(f'El usuario {member} ha sido expulsado por: {reason}')
+    @kick.error
     async def kick_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
-            await ctx.send("No tienes los permisos para hacer esto :)")
+            await ctx.reply("No tienes los permisos para hacer esto :)")
+            
+    # Banear un usuario
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, reason: str | None = "No se dio razón"):
+        await member.ban(reason=reason)
+        await ctx.send(f'El usuario {member} ha sido baneado por: {reason}')
+    @ban.error 
+    async def ban_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.reply("No tienes permisos para hacer esto :)")
+    
 
 # Añadir al cog
 async def setup(bot: commands.Bot):
